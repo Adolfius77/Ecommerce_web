46b90816b51ef96daf3f77a7d4e66b2a196d0af2
angelgbd <baeltranangel@gmail.com>
2026-05-17 18:28:22 -0700

    BOS

---
+++ src/main/java/negocio/interfaces/ICategoriaBO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package negocio.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+import model.Categoria;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author USER
+ */
+public interface ICategoriaBO {
+
+    List<Categoria> listarCategorias();
+
+    Optional<Categoria> obtenerPorId(ObjectId id);
+
+    ObjectId registrarCategoria(Categoria categoria) throws Exception;
+
+    boolean actualizarCategoria(Categoria categoria) throws Exception;
+
+    boolean eliminarCategoria(ObjectId id) throws Exception;
+}
